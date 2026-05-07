473760a220d4dcb3a3b969ebb965090adb535d1c
DSTBP <taddeowang123@gmail.com>
2026-05-07 11:46:30 +0800

    update

--- utils/BoardGame/NineChess-master/tests/Test-Rule0-ChengSanQi.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = "Stop"
-Set-StrictMode -Version Latest
-
-$scriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
-& (Join-Path $scriptRoot "Invoke-RuleHarness.ps1") -RuleIndex 0 -RuleName "Rule 0 - ChengSanQi"
-exit $LASTEXITCODE
