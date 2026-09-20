992ffc73c98640b1bda9ec63ee9d4154c3682112
DSTBP <taddeowang123@gmail.com>
2026-09-20 15:14:09 +0800

    update

--- utils/CoverGen/assets/js/translate-init.js
+++ utils/CoverGen/assets/js/translate-init.js
@@ -25,7 +25,7 @@
     const configureTranslate = translateApi => {
         if (!isTranslateLibrary(translateApi)) return false;
 
-        translateApi.service.use('translate.service');
+        translateApi.service.use('client.edge');
         translateApi.language.setDefaultTo('chinese_simplified');
         translateApi.language.setLocal('chinese_simplified');
         translateApi.listener.start();
